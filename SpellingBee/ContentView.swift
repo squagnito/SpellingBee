e9507ef8f2a49a7e3ac44c56bbd45b13b565b3e5
Aaron Medina <squagnito@gmail.com>
2025-03-07 22:37:15 -0600

    Initial Commit

---
+++ SpellingBee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpellingBee
+//
+//  Created by Aaron Medina on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpellingBee/SpellingBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBeeApp.swift
+//  SpellingBee
+//
+//  Created by Aaron Medina on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
